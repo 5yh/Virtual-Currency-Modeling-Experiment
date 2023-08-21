e245738e62a263c692c2fdd78915c52584d7ead2
root <root@10.160.196.24-node>
2023-08-21 20:40:18 +0800

    modified white data augmentation, but failed

--- zengqiang_train_model.py
+++ zengqiang_train_model.py
@@ -11,11 +11,24 @@ from sklearn import metrics
 from sklearn.utils import shuffle
 # 对黑样本和白样本都进行增强后对模型进行训练
 # 当前是有问题的，还在等白样本增强完成
-train_directoryPath = '/mnt/blockchain0/after_sort/x_train_zengqiang.csv'
+# train_directoryPath = '/mnt/blockchain0/after_sort/x_train_zengqiang.csv'
+# 这个训练的不行，只有1epoch，依托使
+
+# orgin_whitePath= '/mnt/blockchain0/after_sort/x_train_new.csv'
+# 此为根据leaf差值排序后生成的12w行数据，要3000行之后的,参杂的未增强数据不能是排序过的
+origin_whitePath='/mnt/blockchain0/zengliang_test/final_train_data/part-00000-41221f5c-bae3-4859-86c5-60e9c9e65f61-c000.csv'
+train_directoryPath = '/mnt/blockchain0/after_sort/white_augmentation/x_train_zengqiang.csv'
+#这个用前3000条增强的9000条，epoch=300
 train_df = pd.read_csv(train_directoryPath)
 train_df['label'] = 0.0
+train_df2=pd.read_csv(origin_whitePath)
+# train_df2['label'] = 0.0
 
-
+# train_df2=train_df2.loc[3001:60000]
+train_df2 = train_df2.loc[train_df2['label'] == 0]
+train_df2 = train_df2.drop(columns = 'address')
+train_df2=train_df2.sample(frac=0.01, random_state=11451)
+print(train_df2)
 train_blackPath="/mnt/blockchain0/after_sort/black_zengqiang.csv"
 train_black=pd.read_csv(train_blackPath)
 train_black['label']=1.0
@@ -25,10 +38,11 @@ train_black_x = train_black.drop(columns = 'label')
 train_black_y = train_black['label']
 print(type(train_black_y))
 train_all = pd.DataFrame()
-train_all = train_all._append(train_df,ignore_index= True)
+# train_all = train_all._append(train_df,ignore_index= True)
+train_all = train_all._append(train_df2,ignore_index= True)
 train_all = train_all._append(train_black,ignore_index= True)
 train_all=train_all.drop(columns = 'Unnamed: 0')
-train_all = train_all.sample(frac=1, random_state=11451)
+# train_all = train_all.sample(frac=1, random_state=11451)
 print(train_all)
 x_train_new = pd.DataFrame()
 y_train_new = pd.DataFrame()
@@ -48,7 +62,7 @@ test_df = pd.read_csv(test_directoryPath)
 test_df = test_df.drop(columns = 'address')
 x_test= test_df.drop(columns = 'label')
 y_test = test_df['label']
-print(x_test)
+# print(x_test)
 dtest = xgb.DMatrix(x_test,label=y_test,enable_categorical=False)
 
 params_0={'booster':'gbtree',
