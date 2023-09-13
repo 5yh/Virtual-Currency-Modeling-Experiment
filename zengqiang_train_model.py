8d4ec5c37d7a900c2b5ba152decbe102df86722c
root <root@10.160.196.24-node>
2023-09-13 17:18:31 +0800

    增强模型添加保存模型选项

--- zengqiang_train_model.py
+++ zengqiang_train_model.py
@@ -35,6 +35,8 @@ train_all = train_all._append(train_black,ignore_index= True)
 x_train_new = pd.DataFrame()
 y_train_new = pd.DataFrame()
 x_train_new= train_all.drop(columns = 'label')
+drop_columns=['Call_TRC10','Min_value_sent_to_trc10','Max_value_sent_to_trc10','Avg_value_sent_to_trc10','Total_ether_sent_to_trc10','outDegree','inDegree','mean_Call_TRC10','mean_Min_value_sent_to_trc10','mean_Max_value_sent_to_trc10','mean_Avg_value_sent_to_trc10','mean_Total_ether_sent_to_trc10','mean_outDegree','mean_inDegree']
+# x_train_new=x_train_new.drop(columns=drop_columns)
 y_train_new = train_all['label']
 # 使用索引重置保持索引一一对应
 
@@ -49,6 +51,7 @@ test_directoryPath = '/mnt/blockchain0/zengliang_test/final_test_data/part-00000
 test_df = pd.read_csv(test_directoryPath)
 test_df = test_df.drop(columns = 'address')
 x_test= test_df.drop(columns = 'label')
+# x_test=x_test.drop(columns=drop_columns)
 y_test = test_df['label']
 # print(x_test)
 dtest = xgb.DMatrix(x_test,label=y_test,enable_categorical=False)
@@ -65,20 +68,43 @@ params_0={'booster':'gbtree',
     'colsample_bytree':0.8,
     'colsample_bylevel':0.6,
     'min_child_weight':2,
-    'eta': 0.05,
+    'eta': 0.1,
     'seed':0,
     'nthread':16,
      'silent':1,
+     'scale_pos_weight':10
     # 'process_type': 'update',
     # 'updater': 'refresh',
     # 'refresh_leaf': True
     
 }
+# params_0={'booster':'gbtree',
+#     'objective': 'multi:softmax',
+#     'num_class':'3',
+#     # 'objective':'binary:logistic',
+#     'eval_metric': 'auc',
+#     'max_depth':4,
+#     'lambda':1,
+#     'alpha':1,
+#     'subsample':0.8,
+#     'colsample_bytree':0.8,
+#     'colsample_bylevel':0.6,
+#     'min_child_weight':2,
+#     'eta': 0.1,
+#     'seed':0,
+#     'nthread':16,
+#      'silent':1
+#     # 'process_type': 'update',
+#     # 'updater': 'refresh',
+#     # 'refresh_leaf': True
+    
+# }
 watchlist = [(dtrain,'train')]
 # boostArray=np.array([300,400,500,600,1000])
-# boostArray=np.array([350,360,370,380,390])
-boostArray=np.array([1100,1200,1500,1600,2000])
-for i in range(5):
+# boostArray=np.array([10,50,10,150,200,250])
+boostArray=np.array([360])
+# boostArray=np.array([1100,1200,1500,1600,2000])
+for i in range(1):
   print('-----------------------开始进行训练：-----------------------')
   start = time.perf_counter()
   model = xgb.train(params_0, dtrain, num_boost_round=boostArray[i], evals=watchlist)
@@ -86,8 +112,8 @@ for i in range(5):
   print('-----------------------训练结束：-----------------------')
   time_res = end-start
   print('-----------------------增强模型训练所用时间为：',time_res)
-  # model.dump_model("/pub/p1/zengliang_test/model_best.txt")
-  # model.save_model('/pub/p1/zengliang_test/origin_model_best.json')
+  model.dump_model("/mnt/blockchain0/after_sort/bestModel/model_best.txt")
+  model.save_model('/mnt/blockchain0/after_sort/bestModel/origin_model_best.json')
 
 
     # model = xgb.Booster()
